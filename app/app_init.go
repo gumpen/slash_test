690fb7ff7850203354ca46a893709d33194b9b1c
gumpen <shuhei1993727@gmail.com>
2019-09-14 18:49:30 +0900

    Add request body log

--- app/app_init.go
+++ app/app_init.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/favclip/ucon"
@@ -9,6 +11,11 @@ import (
 func AppInit(gae bool) *ucon.ServeMux {
 	ucon.Orthodox()
 	ucon.HandleFunc("POST", "/command", func(w http.ResponseWriter, r *http.Request) {
+		params, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("%#v\n", string(params))
 		w.Write([]byte("Hello World!"))
 	})
 	ucon.DefaultMux.Prepare()
